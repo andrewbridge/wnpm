a1e8c347d082fe228f5b8b1709f9103166adcb9f
Andrew Bridge <andrew@andrewhbridge.co.uk>
2023-01-17 23:18:31 +0000

    chore: switch to jsdelivr for bootstrapping (#4)

--- bookmarklet.js
+++ bookmarklet.js
@@ -5,7 +5,7 @@
     }
     const script = document.createElement('SCRIPT');
     script.type = 'text/javascript';
-    script.src = 'https://gitcdn.link/repo/andrewbridge/wnpm/master/wnpm.min.js';
+    script.src = 'https://cdn.jsdelivr.net/gh/andrewbridge/wnpm@latest/wnpm.min.js';
     script.addEventListener('error', () => alert('An error occurred loading the bookmarklet'), false);
     document.head.appendChild(script);
 })();
